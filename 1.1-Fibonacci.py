662a4cbd6231a67f4bb0f4f258ed7798770ec4d7
Mitch Gollub <mgollub@live.com>
2019-05-19 12:33:39 -0400

    Chapter 1.1

--- 1.1-Fibonacci.py
+++ 1.1-Fibonacci.py
@@ -1,5 +1,9 @@
-from functools import lru_cache #fib4 - caches decorated functions output for set of inputs
-from typing import Dict #fib3
+# fib4 - caches decorated functions output for set of inputs
+from functools import lru_cache
+from typing import Generator  # fib 6
+
+# fib3 - caches function input/output using a dictionary object
+from typing import Dict
 memo: Dict[int, int] = {0: 0, 1: 1}
 
 
@@ -26,7 +30,33 @@ def fib4(n: int) -> int:
     return fib4(n - 2) + fib4(n - 1)
 
 
+def fib5(n: int) -> int:
+    if n == 0:
+        return n  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0  # special case
+    if n > 0:
+        yield 1  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next
+
+
 if __name__ == "__main__":
-    print(fib4(5))
-    print(fib4(10))
-    print(fib4(50))
+    print('Optimized Fibonacci Sequence')
+    print(fib5(5))
+    print(fib5(10))
+    print(fib5(50))
+
+    print('Printing Each step of fib(50)')
+    for i in fib6(50):
+        print(i)
